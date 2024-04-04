6fd719e7e3ba03bd6ea142e48729f9fb5ccd84ab
Aleksandr <snigavik@protonmail.com>
2024-04-04 22:21:38 +0800

    Update beyond.py

--- beyond.py
+++ beyond.py
@@ -1,9 +1,10 @@
 from dataclasses import dataclass
 
-from typing import List, Tuple, ClassVar
+from typing import List, Tuple, Dict, ClassVar
 
+from timeit import timeit
 
-@dataclass(slots=True, frozen=True)
+@dataclass(slots=True)
 class Term:
 	sid: int
 	type: int # 1 is a constat or a functor, 2 is a variable
@@ -11,7 +12,42 @@ class Term:
 	table: ClassVar
 
 	def __str__(self):
-		return f'{Term.table[self.sid]+ ("."+str(self.sid) if self.type==2 else "")}{"" if not self.args else "("+",".join(map(str,self.args))+")"}' 
+		return f'{Term.table[self.sid]+ ("."+str(self.sid) if self.is_variable() else "")}{"" if not self.args else "("+",".join(map(str,self.args))+")"}' 
+
+	def is_variable(self):
+		return self.type == 2
+
+
+@dataclass(slots=True)
+class TQF:
+	quantifier: int # 1 is forall, 2 is exists
+	variables: List
+	conjunct: List
+	next: List
+
+	def is_forall():
+		return self.quantifier == 1
+
+	def is_exists():
+		return self.quantifier == 2
+
+
+@daaclass(slots=True)
+class Question:
+	tqf,
+	context,
+	 
+
+@dataclass(slots=True)
+class Context:
+	m: Dict
+
+
+
+
+
+
+
 
 
 Term.table = {1:"a", 2:"x",3:"f",4:"h"}
