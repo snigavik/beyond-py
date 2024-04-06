b3fc5615fa291f2374ea02f6e2ac7c6cfc01ba1a
Aleksandr <snigavik@protonmail.com>
2024-04-06 10:19:32 +0800

    Update beyond.py

--- beyond.py
+++ beyond.py
@@ -4,15 +4,19 @@ from typing import List, Tuple, Dict, ClassVar, Any
 
 from timeit import timeit
 
-@dataclass(slots=True)
+@dataclass(slots=True, unsafe_hash=True)
 class Term:
 	sid: int
 	type: int # 1 is a constat or a functor, 2 is a variable
 	args: Tuple
 	table: ClassVar
+	print_mode: ClassVar = "plain"
 
 	def __str__(self):
-		return f'{Term.table[self.sid]+ ("."+str(self.sid) if self.is_variable() else "")}{"" if not self.args else "("+",".join(map(str,self.args))+")"}' 
+		if Term.print_mode == "varbose":
+			return f'{Term.table[self.sid]+ ("."+str(self.sid) if self.is_variable() else "")}{"" if not self.args else "("+",".join(map(str,self.args))+")"}' 
+		if Term.print_mode == "plain":
+			return f'{Term.table[self.sid]}{"" if not self.args else "("+",".join(map(str,self.args))+")"}' 
 
 	def is_variable(self):
 		return self.type == 2
@@ -35,6 +39,10 @@ class VMap:
 
 	def add(self, v, t):
 		self.m[v] = t
+
+	def __str__(self):
+		return "{" + ", ".join(map(lambda p: str(p[0]) + " -> " + str(p[1]), self.m.items())) + "}"
+
 
 
 
@@ -65,11 +73,11 @@ class Question:
 
 
 
-def match(t_a: Term, t_e: Term, a_context: VMap, curr_answer: VMap, curr_map):
+def match(t_a: Term, t_e: Term, a_context: VMap, curr_answer: VMap, curr_map: VMap):
 	if t_a.is_variable():
-		if a_context.check_v(t_a): return match(a_context.get_v(t_a), t_e, a_context, curr_answer, curr_map)
-		if curr_answer.check_v(t_a): return match(curr_answer.get_v(t_a), t_e, a_context, curr_answer, curr_map)
-		if curr_map.check_v(t_1): return match(curr_map.get_v(t_a), t_e, a_context, curr_answer, curr_map)
+		if a_context.check_v(t_a): return match(a_context.get_map(t_a), t_e, a_context, curr_answer, curr_map)
+		if curr_answer.check_v(t_a): return match(curr_answer.get_map(t_a), t_e, a_context, curr_answer, curr_map)
+		if curr_map.check_v(t_a): return match(curr_map.get_map(t_a), t_e, a_context, curr_answer, curr_map)
 		curr_map.add(t_a,t_e)
 		return True
 	if t_a.sid == t_e.sid:
@@ -85,20 +93,40 @@ def match(t_a: Term, t_e: Term, a_context: VMap, curr_answer: VMap, curr_map):
 
 
 
-Term.table = {1:"a", 2:"x",3:"f",4:"h"}
+Term.table = {1:"a", 2:"x",3:"f",4:"h",5:"e", 6:"y"}
 
 t1 = Term(1,1,())
 t1x = Term(1,1,())
 t2 = Term(2,2,())
+t_e = Term(5,1,())
 
 t3 = Term(3,1,(t1,t1x,t2))
-t4 = Term(3,1,(t1x,t1x,t2))
+t4 = Term(3,1,(t1x,t1x,t_e))
 
-t5 = Term(4,1,(t3,t2))
-t6 = Term(4,1,(t4,t2))
+t_y = Term(6,2,())
+
+t5 = Term(4,1,(t3,t_y))
+t6 = Term(4,1,(t4,t1))
 
 
 print(t5)
 print(t6)
 print(t5 == t6)
 print(Term.table)
+
+r = VMap({})
+match(t5, t6, VMap({}), VMap({}), r)
+print(r)
+
+
+
+
+#
+
+
+
+
+
+
+
+#
